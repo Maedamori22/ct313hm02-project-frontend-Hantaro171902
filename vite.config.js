f8b46d3b381410d0f35fb2a8b13c2add4d44f4c8
Hantaro191702 <hanb2014913@student.ctu.edu.vn>
2023-11-09 22:44:08 +0700

    updated by han

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,19 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000/',
+        changeOrigin: true,
+      },
+    },
+  },
 })
