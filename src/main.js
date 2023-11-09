4fdf2db1fb5053ee28cd83fe6cc42b98483de4c8
Hantaro191702 <hanb2014913@student.ctu.edu.vn>
2023-11-09 23:02:03 +0700

    updated by han

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import App from "./App.vue";
-import router from "./router";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import App from './App.vue';
+// import router from "./router";
 
 createApp(App).mount('#app');
