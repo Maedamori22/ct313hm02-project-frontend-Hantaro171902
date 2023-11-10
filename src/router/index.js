12f02a87531b0e434ef6e1e9b39d4a05e5602839
Hantaro191702 <hanb2014913@student.ctu.edu.vn>
2023-11-10 22:19:12 +0700

    updated by Han

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,22 @@
+import { createWebHistory, createRouter } from 'vue-router';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
-import Menu from '../views/Menu.vue';
-import AboutUs from '../views/AboutUs.vue';
-import Cart from '../views/Cart.vue';
+import Home from '@/views/Home.vue';
+// import Menu from '../views/Menu.vue';
+// import AboutUs from '../views/AboutUs.vue';
+// import Cart from '../views/Cart.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/menu', component: Menu },
-  { path: '/about', component: AboutUs },
-  { path: '/cart', component: Cart }
+  { path: '/', component: Home }
+  //   { path: '/menu', component: Menu },
+  //   { path: '/about', component: AboutUs },
+  //   { path: '/cart', component: Cart }
 ];
 
-const router = new VueRouter({
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
 });
 
