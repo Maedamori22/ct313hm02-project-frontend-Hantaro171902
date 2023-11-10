f8ca770d94e2f77db2c03856b9e487160a97f700
Hantaro191702 <hanb2014913@student.ctu.edu.vn>
2023-11-10 21:09:29 +0700

    updated by Han

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1>Hello, Vue.js</h1>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style>
+/* Add your styles here */
+</style>
