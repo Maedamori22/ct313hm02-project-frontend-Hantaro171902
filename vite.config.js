c2ca2cf273e0b6289476d645b4fca9732fc552e4
Hantaro191702 <hanb2014913@student.ctu.edu.vn>
2023-11-10 20:39:57 +0700

    updated by han

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  
+  plugins: [vue()],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -19,8 +17,20 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://localhost:3000/',
-        changeOrigin: true,
-      },
-    },
+        changeOrigin: true
+      }
+    }
   },
-})
+
+  middleware: (customMiddleware) => {
+    return [
+      (req, res, next) => {
+        if (req.url.startsWith('/api')) {
+          console.log(`Request to backend: ${req.method} ${req.url}`);
+        }
+        next();
+      },
+      customMiddleware
+    ];
+  }
+});
