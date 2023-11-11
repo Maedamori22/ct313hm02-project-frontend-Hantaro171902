f1b98c2c6d53ba2567b1cd1421692f50b3677884
Hantaro191702 <hanb2014913@student.ctu.edu.vn>
2023-11-11 08:56:50 +0700

    updated by Han

---
+++ src/components/Menu.vue
@@ -0,0 +1,34 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    <h1>Menu</h1>
+
+    <div v-if="menuItems.length">
+      <ul>
+        <li v-for="(item, index) in menuItems" :key="index">{{ item.name }} - {{ item.price }}</li>
+      </ul>
+    </div>
+
+    <div v-else>
+      <p>No items available in the menu.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuItems: [
+        { name: 'Item 1', price: '$10.99' },
+        { name: 'Item 2', price: '$8.99' }
+        // Add more menu items as needed
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+/* Add component-specific styles here */
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,31 +2,27 @@
 <template>
   <nav class="navbar navbar-expand-md navbar-dark bg">
     <div class="container-fluid">
-      <img class="navbar-brand fs-3 ms-3" src="@/assets/picture/White_logo800.png" alt="Logo" />
+      <router-link to="/"
+        ><img class="navbar-brand ms-3" src="@/assets/picture/White_logo800.png" alt="Logo"
+      /></router-link>
 
-      <div class="collapse navbar-collapse" id="btn">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button class="btn btn-outline" type="submit">
+          <i class="fa fa-search" aria-hidden="true"></i>
+        </button>
+      </form>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
           <li class="nav-item">
-            <form class="d-flex" role="search">
-              <input
-                class="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button class="btn btn-outline" type="submit">
-                <i class="fa fa-search" aria-hidden="true"></i>
-              </button>
-            </form>
+            <router-link to="/" class="nav-link mx-3 text-white fs-5">Home</router-link>
           </li>
           <li class="nav-item">
-            <router-link to="/" class="nav-link mx-3">Home</router-link>
+            <router-link to="/menu" class="nav-link mx-3 text-white fs-5">Menu</router-link>
           </li>
           <li class="nav-item">
-            <router-link to="/menu" class="nav-link mx-3">Menu</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/about" class="nav-link mx-3">About Us</router-link>
+            <router-link to="/about" class="nav-link mx-3 text-white fs-5">About Us</router-link>
           </li>
           <li class="nav-item">
             <router-link to="/cart" class="nav-link mx-3">
@@ -42,8 +38,75 @@
           </li>
         </ul>
       </div>
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+      >
+        <i class="fa fa-bars"></i>
+      </button>
     </div>
   </nav>
+
+  <div id="carouselExampleIndicators" class="carousel slide">
+    <div class="carousel-indicators">
+      <button
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide-to="0"
+        class="active"
+        aria-current="true"
+        aria-label="Slide 1"
+      ></button>
+      <button
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide-to="1"
+        aria-label="Slide 2"
+      ></button>
+      <button
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide-to="2"
+        aria-label="Slide 3"
+      ></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="@/assets/picture/COVER_WEB_2000x878.jpg" class="d-block w-100" alt="cover" />
+      </div>
+      <div class="carousel-item">
+        <img src="@/assets/picture/DIGITAL_Website_1440x460.jpg" class="d-block w-100" alt="..." />
+      </div>
+      <div class="carousel-item">
+        <img
+          src="@/assets/picture/HC_Brand_Refresh_Master_KV_1440x460.png"
+          class="d-block w-100"
+          alt="..."
+        />
+      </div>
+    </div>
+    <button
+      class="carousel-control-prev"
+      type="button"
+      data-bs-target="#carouselExampleIndicators"
+      data-bs-slide="prev"
+    >
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button
+      class="carousel-control-next"
+      type="button"
+      data-bs-target="#carouselExampleIndicators"
+      data-bs-slide="next"
+    >
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
 </template>
 
 <script>
@@ -76,36 +139,17 @@ export default {
 
 .navbar-brand {
   /* Style for logo or brand content */
-  height: 100px;
-  width: 100px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  height: 50px;
+  width: 50px;
 }
 
 .navbar-links {
   display: flex;
 }
 
-.router-link {
-  text-decoration: none;
-  color: #fff;
-  margin: 0 15px;
-  cursor: pointer;
-}
-
-.navbar-auth {
-  /* Style for authentication button or icon */
-}
-
 /* Media query for responsiveness */
-@media (max-width: 768px) {
-  .navbar-links {
-    flex-direction: column;
-    align-items: center;
-  }
-
-  .router-link {
-    margin: 10px 0;
-  }
-}
 
 /* Import external styles */
 /* @import '@/assets/css/navbar.css'; */
